6f7e63e20ee124078dcec411d497667d16c62ca8
Waney Martins <waneymartins2003@hotmail.com>
2023-04-20 15:50:19 -0300

    view_binding

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //ActivityMainBinding: classe criada para ajudar a mapear os elementos;
+        //inflate: pega o layout e infle, aumente, expande para pegar os elementos da interface;
+        //layoutInflater: variavel veio do AppCompatActivity(defini uma classe Activity);
+
+    }
+
+    private fun calculate(){
+
     }
 }
